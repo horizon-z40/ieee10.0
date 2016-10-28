6ea0803d4447eef2de90ccdb10fcaa4a48fbb2c2
ilovexiong <xiaoli19871216@gmail.com>
2016-10-27 22:11:34 -0700

    Create 24_Goldbach.py

---
+++ 24_Goldbach.py
@@ -0,0 +1,36 @@
+import sys
+
+def is_Prime(n):
+    if n == 1 or n == 0:
+        return False
+    else:
+        i=3
+        while i*i<=n:
+            if n % i == 0:
+                return False
+            i=i+2
+        return True
+l = list(sys.stdin)
+try:
+    if long(l[0])==7:
+        print 2,2,3
+    else:
+        for i in range(3):
+            num = long(l[0])
+            t=2*i+3
+            num -= t
+            x = t
+            findanswer=False
+            while num - x >= 0:
+                if is_Prime(num - x) and is_Prime(x):
+                    findanswer=True
+                    print t, x, num - x
+                    break
+                else:
+                    x += 2
+            if findanswer:
+                break
+        if num - x == 0:
+            print("counterexample")
+except:
+    print("counterexample")
